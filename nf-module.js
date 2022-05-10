04f12698accf158264ba94a57b50f2571ddc8af3
Azat Yamalov <yamalov.azat@live.com>
2022-05-10 22:18:37 +0300

    removed default init

--- nf-module.js
+++ nf-module.js
@@ -1,4 +1,3 @@
-import * as i18n from "i18n";
 import path from 'path';
 import { addLocaleFile, getCatalog } from "./lib/localize.js";
 import { registerLibDir } from "@nfjs/front-server";
@@ -7,11 +6,6 @@ const __dirname = path.join(path.dirname(decodeURI(new URL(import.meta.url).path
 
 export async function init() {
     registerLibDir('sprintf-js');
-
-    i18n.default.configure({
-        defaultLocale: 'ru'
-    })
-
     addLocaleFile('ru', path.join(__dirname, 'locales', 'ru.json'))
 
     web.on('POST', '/i18n_catalog', { middleware: ['json'] }, getCatalog)
